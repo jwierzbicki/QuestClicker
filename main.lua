9f57c349e921cb3cab6c3c527b8010860cf68aad
jwierzbicki <jwierzbicki22@gmail.com>
2019-11-09 10:37:24 +0100

    Remove setting event flags, do quest stuff automatically at event

--- main.lua
+++ main.lua
@@ -35,44 +35,75 @@ function SlashCommandHandler(msg, editbox)
 	end
 end
 
--- TODO: accepting, completing quest logic
 function DoQuestThing(self, button, down)
-	-- local isCompletable = IsQuestCompletable()
 	if eventFlags["GOSSIP_SHOW"] == 1 then
 		local numNewQuests = GetNumGossipAvailableQuests()
 		local numActiveQuests = GetNumGossipActiveQuests()
-		local numOptions = GetNumGossipOptions()
+		-- local numOptions = GetNumGossipOptions()
 		print("new quest number: " .. numNewQuests)
 		print("active quest number: " .. numActiveQuests)
-		print("options number: " .. numOptions)
-		print("Quests details:")
-		for i=0, numNewQuests, 1 do
-			local title = GetAvailableTitle(i) -- this doesn't work
-			print("Quest" .. i .. " title: " .. title)
-		end
-	end
-	-- print("is completable: " .. isCompletable)
-	-- SelectAvailableQuest()
+		-- print("options number: " .. numOptions)
 
-	-- print("eventFlags[QUEST_DETAIL]: " .. eventFlags["QUEST_DETAIL"])
+		-- title1, level1, isLowLevel1, isComplete1, title2, level2, isLowLevel2, isComplete2 = GetGossipActiveQuests()
+		-- print("Quest1: " .. title1 .. " " .. level1 .. " " .. isLowLevel1 .. " " .. isComplete1)
+		-- print("Quest2: " .. title2 .. " " .. level2 .. " " .. isLowLevel2 .. " " .. isComplete2)
+		-- print("Quests details:")
+		-- for i=0, numNewQuests, 1 do
+		-- 	local title = GetAvailableTitle(i) -- this doesn't work
+		-- 	print("Quest" .. i .. " title: " .. title)
+		-- end
+
+		SelectGossipAvailableQuest(1) -- works!
+	end
+
+	-- SelectAvailableQuest(0) -- doesn't work
+
+	-- Accept when selected (QUEST_DETAIL event fired)
 	if eventFlags["QUEST_DETAIL"] == 1 then
-		print("Quest title: " .. GetTitleText()) -- this one works
+		print("Accepting quest: " .. GetTitleText()) -- this one works
 		AcceptQuest()
 	end
 end
 
-function QuestGreetingHandler(self, event, ...)
+function QuestEventHandler(self, event, ...)
 	print("Quest event " .. event)
-	if event == "QUEST_DETAIL" then
-		eventFlags["QUEST_DETAIL"] = 1
-	elseif event == "GOSSIP_SHOW" then
-		eventFlags["GOSSIP_SHOW"] = 1
-		-- reset all event flags
-	elseif event == "GOSSIP_CLOSED" or event == "QUEST_FINISHED" then
-		for key, value in pairs(eventFlags) do
-			eventFlags[key] = 0
+	local numNewQuests = GetNumGossipAvailableQuests()
+	if numNewQuests > 0 then
+		if event == "GOSSIP_SHOW" then
+			SelectGossipAvailableQuest(1) -- 1 for now
+		end
+		if event == "QUEST_DETAIL" then
+			AcceptQuest() -- window is kept opened after accepting last quest if there are more than 1 available
 		end
 	end
+
+	local numActiveQuests = GetNumGossipActiveQuests()
+	if numActiveQuests > 0 then
+		if event == "GOSSIP_SHOW" then
+			title1, level1, isLowLevel1, isComplete1, isLegendary1, isIgnored1, title2, level2, isLowLevel2, isComplete2, isLegendary2, isIgnored2 = GetGossipActiveQuests()
+			if isComplete1 then
+				SelectGossipActiveQuest(1)
+			elseif isComplete2 then
+				SelectGossipActiveQuest(2)
+			end
+		end
+	end
+
+	-- if numNewQuests == 0 then -- automatically closes the window, this is not the solution
+	-- 	CloseGossip()
+	-- end
+
+	-- old version with event flags, to remove
+	-- if event == "QUEST_DETAIL" then
+	-- 	eventFlags["QUEST_DETAIL"] = 1
+	-- elseif event == "GOSSIP_SHOW" then
+	-- 	eventFlags["GOSSIP_SHOW"] = 1
+	-- 	-- reset all event flags
+	-- elseif event == "GOSSIP_CLOSED" or event == "QUEST_FINISHED" then
+	-- 	for key, value in pairs(eventFlags) do
+	-- 		eventFlags[key] = 0
+	-- 	end
+	-- end
 end
 
 eventFlags = {}
@@ -100,14 +131,14 @@ EventFrame:SetScript("OnEvent", LoginEventHandler)
 -- Quest events
 local questEventsFrame = CreateFrame("Frame")
 questEventsFrame:RegisterEvent("GOSSIP_CLOSED")
-questEventsFrame:RegisterEvent("GOSSIP_CONFIRM")
-questEventsFrame:RegisterEvent("GOSSIP_CONFIRM_CANCEL")
-questEventsFrame:RegisterEvent("GOSSIP_ENTER_CODE")
+-- questEventsFrame:RegisterEvent("GOSSIP_CONFIRM") -- didn't trigger yet
+-- questEventsFrame:RegisterEvent("GOSSIP_CONFIRM_CANCEL") -- didn't trigger yet
+-- questEventsFrame:RegisterEvent("GOSSIP_ENTER_CODE") -- no idea when it triggers
 questEventsFrame:RegisterEvent("GOSSIP_SHOW")
-questEventsFrame:RegisterEvent("QUEST_GREETING")
+-- questEventsFrame:RegisterEvent("QUEST_GREETING") -- doesn't ever trigger 
 questEventsFrame:RegisterEvent("QUEST_DETAIL")
-questEventsFrame:RegisterEvent("QUEST_CHOICE_CLOSE")
+-- questEventsFrame:RegisterEvent("QUEST_CHOICE_CLOSE") -- not needed, maybe triggers on reward choice
 questEventsFrame:RegisterEvent("QUEST_FINISHED")
 questEventsFrame:RegisterEvent("QUEST_PROGRESS")
 questEventsFrame:RegisterEvent("QUEST_ACCEPTED")
-questEventsFrame:SetScript("OnEvent", QuestGreetingHandler)
+questEventsFrame:SetScript("OnEvent", QuestEventHandler)
