130649932f33a6cf07f227328b6be65f144f5d56
jwierzbicki <jwierzbicki22@gmail.com>
2019-11-09 10:39:02 +0100

    Remove old commented code, remove tutorial code

--- main.lua
+++ main.lua
@@ -1,27 +1,3 @@
-function LoginEventHandler(self,event,...) 
-	if type(qclickSavedVar) ~= "table" then
-		qclickSavedVar = {}
-		qclickSavedVar[UnitName("Player")] = 1
-		ChatFrame1:AddMessage('QuestClicker: Hello ' .. UnitName("Player") .. ', we\'re meeting first time!')
-	else
-		if qclickSavedVar[UnitName("Player")] == 1 then
-			ChatFrame1:AddMessage('QuestClicker: Hello ' .. UnitName("Player") .. ', i\'ve seen you ' .. qclickSavedVar[UnitName("Player")] .. ' time before!')
-		else
-			ChatFrame1:AddMessage('QuestClicker: Hello ' .. UnitName("Player") .. ', i\'ve seen you ' .. qclickSavedVar[UnitName("Player")] .. ' times before!')
-		end
-		local found = 0
-		for name, number in pairs(qclickSavedVar) do
-			if UnitName("Player") == name then
-				qclickSavedVar[name] = qclickSavedVar[name] + 1
-				found = 1
-			end
-		end
-		if found == 0 then
-			qclickSavedVar[UnitName("Player")] = 1
-		end
-	end
-end
-
 function SlashCommandHandler(msg, editbox)
 	local _, _, cmd, args = string.find(msg, "%s?(%w+)%s?(.*)") -- parse string to find command and args
 	if cmd == "set" and args ~= "" then
@@ -36,33 +12,7 @@ function SlashCommandHandler(msg, editbox)
 end
 
 function DoQuestThing(self, button, down)
-	if eventFlags["GOSSIP_SHOW"] == 1 then
-		local numNewQuests = GetNumGossipAvailableQuests()
-		local numActiveQuests = GetNumGossipActiveQuests()
-		-- local numOptions = GetNumGossipOptions()
-		print("new quest number: " .. numNewQuests)
-		print("active quest number: " .. numActiveQuests)
-		-- print("options number: " .. numOptions)
-
-		-- title1, level1, isLowLevel1, isComplete1, title2, level2, isLowLevel2, isComplete2 = GetGossipActiveQuests()
-		-- print("Quest1: " .. title1 .. " " .. level1 .. " " .. isLowLevel1 .. " " .. isComplete1)
-		-- print("Quest2: " .. title2 .. " " .. level2 .. " " .. isLowLevel2 .. " " .. isComplete2)
-		-- print("Quests details:")
-		-- for i=0, numNewQuests, 1 do
-		-- 	local title = GetAvailableTitle(i) -- this doesn't work
-		-- 	print("Quest" .. i .. " title: " .. title)
-		-- end
-
-		SelectGossipAvailableQuest(1) -- works!
-	end
-
-	-- SelectAvailableQuest(0) -- doesn't work
-
-	-- Accept when selected (QUEST_DETAIL event fired)
-	if eventFlags["QUEST_DETAIL"] == 1 then
-		print("Accepting quest: " .. GetTitleText()) -- this one works
-		AcceptQuest()
-	end
+	print("DoQuestThing called")
 end
 
 function QuestEventHandler(self, event, ...)
@@ -92,18 +42,6 @@ function QuestEventHandler(self, event, ...)
 	-- if numNewQuests == 0 then -- automatically closes the window, this is not the solution
 	-- 	CloseGossip()
 	-- end
-
-	-- old version with event flags, to remove
-	-- if event == "QUEST_DETAIL" then
-	-- 	eventFlags["QUEST_DETAIL"] = 1
-	-- elseif event == "GOSSIP_SHOW" then
-	-- 	eventFlags["GOSSIP_SHOW"] = 1
-	-- 	-- reset all event flags
-	-- elseif event == "GOSSIP_CLOSED" or event == "QUEST_FINISHED" then
-	-- 	for key, value in pairs(eventFlags) do
-	-- 		eventFlags[key] = 0
-	-- 	end
-	-- end
 end
 
 eventFlags = {}
@@ -122,22 +60,14 @@ qclickBtn = CreateFrame("BUTTON", "MyBindingHandlingButton") -- "BUTTON" here is
 -- 	SetBindingClick(qclickSavedVar.keybind, qclickBtn:GetName())
 -- 	print("QClick: previously saved keybind: " .. qclickSavedVar.keybind)
 -- end
-qclickBtn:SetScript("OnClick", DoQuestThing) -- set handler for keybind click
 
-local EventFrame = CreateFrame("Frame")
-EventFrame:RegisterEvent("PLAYER_LOGIN")
-EventFrame:SetScript("OnEvent", LoginEventHandler)
+qclickBtn:SetScript("OnClick", DoQuestThing) -- set handler for keybind click
 
 -- Quest events
 local questEventsFrame = CreateFrame("Frame")
 questEventsFrame:RegisterEvent("GOSSIP_CLOSED")
--- questEventsFrame:RegisterEvent("GOSSIP_CONFIRM") -- didn't trigger yet
--- questEventsFrame:RegisterEvent("GOSSIP_CONFIRM_CANCEL") -- didn't trigger yet
--- questEventsFrame:RegisterEvent("GOSSIP_ENTER_CODE") -- no idea when it triggers
 questEventsFrame:RegisterEvent("GOSSIP_SHOW")
--- questEventsFrame:RegisterEvent("QUEST_GREETING") -- doesn't ever trigger 
 questEventsFrame:RegisterEvent("QUEST_DETAIL")
--- questEventsFrame:RegisterEvent("QUEST_CHOICE_CLOSE") -- not needed, maybe triggers on reward choice
 questEventsFrame:RegisterEvent("QUEST_FINISHED")
 questEventsFrame:RegisterEvent("QUEST_PROGRESS")
 questEventsFrame:RegisterEvent("QUEST_ACCEPTED")
